f4bb696336d2f84f034803fd01bfa67aa49d3aca
gustavopimentelrinaldi <gustavorinaldiiii@gmail.com>
2018-04-04 22:20:15 -0300

    db setting

--- application/db/schema.rb
+++ application/db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 20171023172558) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "adminpack"
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
